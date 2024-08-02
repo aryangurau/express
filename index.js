e89ea81528894dba1649679e9a6ab0939a95d5cb
aryangurau <aryangurau143@gmail.com>
2024-08-02 09:51:38 +0545

    practiced to create server in express

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ app.get("/", (req, res) => {
 app.listen(5666, () => {
   console.log("server is running on port 5666");
 });
+
+//practiced express
+/*
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("this is express server");
+});
+app.listen(5555, () => {
+  console.log("the app server is runnning on port 5555");
+});*/
